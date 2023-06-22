10640ff2f4e01a68c3032a01c8667ed6dd9bd40b
Erwan MARTIN <73824807+R1leMargoulin@users.noreply.github.com>
2023-06-22 12:10:27 +0200

    Create kill.py

---
+++ kill.py
@@ -0,0 +1,22 @@
+import subprocess as sub
+import signal
+import os
+import time
+
+def kill_process_and_sub(process):
+    pids = [process.pid] #we create a list to put all the pid here
+    for l in sub.check_output(["pgrep", "-P",str(process.pid)]).split(b'\n'): #we catch all the PIDs of the subprocess to put them in the pids list
+        #print(l)
+        if(l != b''):
+            pids.append(int(float(l)))
+    for pid in pids: #foreach pids we kill the associated process
+        print("killing "+str(pid))
+        os.kill(pid, signal.SIGTERM)
+        
+        
+#---MAIN---
+if __name__ == '__main__':
+    proc = sub.Popen(["COMMAND", "ARG1", "ARG2", "..."])
+    time.sleep(5)
+    kill_process_and_sub(proc)
+  
